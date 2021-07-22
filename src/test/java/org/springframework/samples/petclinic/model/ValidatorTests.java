a4b388255eb7fa79ba5d84ca7351e6bf3d2be47d
root <root@MT-CENTOS.lul1a142ujne1no4vkacsxaveg.zx.internal.cloudapp.net>
2021-07-22 21:46:21 +0000

    break a test mt

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("notempty01");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
